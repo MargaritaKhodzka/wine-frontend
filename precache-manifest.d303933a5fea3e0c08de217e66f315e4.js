52e34496213519f44ce5dbe6ffba5d871271a534
MargaritaKhodzka <>
2022-02-12 21:01:22 -0500

    Updates

---
+++ precache-manifest.d303933a5fea3e0c08de217e66f315e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f2e20a04076ba4c13e2d1e5783302e",
+    "url": "/wine-frontend/index.html"
+  },
+  {
+    "revision": "cf225f9de97822633683",
+    "url": "/wine-frontend/static/css/2.b902c85c.chunk.css"
+  },
+  {
+    "revision": "52988cd68cbffcf570eb",
+    "url": "/wine-frontend/static/css/main.42b364dd.chunk.css"
+  },
+  {
+    "revision": "cf225f9de97822633683",
+    "url": "/wine-frontend/static/js/2.ab87823d.chunk.js"
+  },
+  {
+    "revision": "04f8113689b490503efd2991dc5cbb53",
+    "url": "/wine-frontend/static/js/2.ab87823d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52988cd68cbffcf570eb",
+    "url": "/wine-frontend/static/js/main.3ba84891.chunk.js"
+  },
+  {
+    "revision": "067672409f72923ab978",
+    "url": "/wine-frontend/static/js/runtime-main.3348db71.js"
+  }
+]);
