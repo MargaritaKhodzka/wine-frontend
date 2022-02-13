75069c97e46d61462da75d1204425ce3117ac899
MargaritaKhodzka <>
2022-02-12 21:49:44 -0500

    Updates

---
+++ precache-manifest.3b910b067cec73593dba4a0c9c68d021.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202a5920e563dc0dc4152ee21aafbb23",
+    "url": "/MargaritaKhodzka/wine-frontend/settings/pages/index.html"
+  },
+  {
+    "revision": "c0b627c86085e7cdfa3a",
+    "url": "/MargaritaKhodzka/wine-frontend/settings/pages/static/css/2.b902c85c.chunk.css"
+  },
+  {
+    "revision": "10ff75f6c8cce35c3bbc",
+    "url": "/MargaritaKhodzka/wine-frontend/settings/pages/static/css/main.42b364dd.chunk.css"
+  },
+  {
+    "revision": "c0b627c86085e7cdfa3a",
+    "url": "/MargaritaKhodzka/wine-frontend/settings/pages/static/js/2.d6a3655b.chunk.js"
+  },
+  {
+    "revision": "04f8113689b490503efd2991dc5cbb53",
+    "url": "/MargaritaKhodzka/wine-frontend/settings/pages/static/js/2.d6a3655b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10ff75f6c8cce35c3bbc",
+    "url": "/MargaritaKhodzka/wine-frontend/settings/pages/static/js/main.f8ff83fa.chunk.js"
+  },
+  {
+    "revision": "b992631eddcfb9c4f206",
+    "url": "/MargaritaKhodzka/wine-frontend/settings/pages/static/js/runtime-main.9fbba2e7.js"
+  }
+]);
